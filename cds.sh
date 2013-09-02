493bebaaaa357942bd3894e7f50b9fa687ee428f
Ashod Ayanyan <aayanyan@gmail.com>
2013-09-02 14:07:51 -0700

    cds fix

--- cds.sh
+++ cds.sh
@@ -1,25 +1,23 @@
 #Work In Progress
 function cds {
 	SEARCH=$1
-	MATCH=$( ls -d */ | grep ^$1$ -c )
+	EXACTMATCH=$( ls -d */ | grep ^$1$ -c )
 	ANY=$( ls -d */ | grep -i ^$1.*$2$ -c )
-	if [ $MATCH == "1" ]; 
+	IFS='/
+	'
+	if [ $EXACTMATCH == "1" ]; 
 		then cd $1		
 	elif [ "$SEARCH" -eq "$SEARCH" ] 2>/dev/null; then
 		array=(`ls -d */ `)
-		IFS='
-		'
 		cd "${array[ $SEARCH - 1 ]}"
 	else
 		if [ "$ANY" == "1" ] ;
 			then
 			cd $( ls -d */ | grep -i ^$1.*$2$ )
 		else 
-			IFS='
-			'
+		 	
 			array=( $( ls -d */ | grep -o -i ^$1.*$2$ ) )
 			index=1
-
 			for i in "${array[@]}" 
 			do
 				echo -e "\033[32m$index\033[39m: $i"
@@ -34,4 +32,6 @@ function cds {
 			fi
 		fi
 	fi
+	IFS='
+	'
 }
