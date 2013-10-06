177139f4e2f06dd2a8cf488f81ebfa63dca011bd
Ashod <ashod@Ashods-MacBook-Air.local>
2013-10-05 21:52:09 -0700

    added the ability to list and cd into hidden folders

--- cds.sh
+++ cds.sh
@@ -1,22 +1,31 @@
 #Work In Progress
 function cds {
 	SEARCH=$1
-	EXACTMATCH=$( ls -d */ | grep ^$1$ -c )
-	ANY=$( ls -d */ | grep -i ^$1.*$2$ -c )
+	EXACTMATCH_COUNT=$( ls -d */ .*/ | grep ^$1$ -c )
+	PARTIALMATCH_COUNT=$( ls -d */ .*/ | grep -i ^$1.*$2/$ -c )
 	IFS='/
 	'
-	if [ $EXACTMATCH == "1" ]; 
-		then cd $1		
-	elif [ "$SEARCH" -eq "$SEARCH" ] 2>/dev/null; then
-		array=(`ls -dt */ `)
+	
+	# If input is numeric
+	if [ "$SEARCH" -eq "$SEARCH" ] 2>/dev/null; then
+		array=(`ls -d */ `)
 		cd "${array[ $SEARCH - 1 ]}"
+	# If input matches a directory name
+	elif [ $EXACTMATCH_COUNT == "1" ]; then 
+		cd $1		
 	else
-		if [ "$ANY" == "1" ] ;
+		# If partial match of one directory
+		if [ $PARTIALMATCH_COUNT == "1" ] ;
 			then
-			cd $( ls -dt */ | grep -i ^$1.*$2$ )
-		else 
-		 	
-			array=( $( ls -dt */ | grep -o -i ^$1.*$2$ ) )
+			cd $( ls -d */ .*/ | grep -i ^$1.*$2/$ )
+		# If partial match of one directory	
+		elif [ "$PARTIALMATCH_COUNT" -gt "1" ] ; then
+			if [[ "$1" =~ ^\..* ]]; then
+				array=( $( ls -d */ .*/ | grep -o -i ^$1.*$2/$ ) )
+			else
+				array=( $( ls -d */ | grep -o -i ^$1.*$2/$ ) )
+			fi
+			
 			index=1
 			for i in "${array[@]}" 
 			do
@@ -29,7 +38,9 @@ function cds {
 
 			if [ "$DIR" != "" ]
 				then cd "${array[$(( $DIR - 1 ))]}"
-			fi
+			fi	
+		else 
+			cd $1	
 		fi
 	fi
 	IFS='
