77083a8196c1d17466e83179c99e1761de9f56a6
Ashod Ayanyan <aayanyan@gmail.com>
2013-10-06 00:35:58 -0700

    update to setup comment

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 BASH_PROFILE=~/.bash_profile
 
-echo -e '\n# .base_terminal - https://github.com/ashoda/.base_terminal.git' >> "$BASH_PROFILE"
+echo -e '\n#.base_terminal - Shortcuts and Bash Utilities - https://github.com/ashoda/.base_terminal.git' >> "$BASH_PROFILE"
 echo 'source ~/.base_terminal/base.sh' >> "$BASH_PROFILE"
