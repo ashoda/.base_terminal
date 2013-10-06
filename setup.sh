557ccdc7e68a18cd07ce500eaad04008985a99d3
Ashod <ashod@Ashods-MacBook-Air.local>
2013-10-06 00:13:57 -0700

    added setup script

---
+++ setup.sh
@@ -0,0 +1,4 @@
+BASH_PROFILE=~/.bash_profile
+
+echo -e '\n# .base_terminal - https://github.com/ashoda/.base_terminal.git' >> "$BASH_PROFILE"
+echo 'source ~/.base_terminal/base.sh' >> "$BASH_PROFILE"
