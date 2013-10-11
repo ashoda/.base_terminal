c2d407fcbf246c8b3833fb3affcfb420f7cb5e10
Ashod <ashod@Ashods-MacBook-Air.local>
2013-10-10 23:29:03 -0700

    update to heroku helpers and updated README

--- README.md
+++ README.md
@@ -23,6 +23,23 @@ Basic dev utilities and shortcuts for common and useful terminal functionality.
 
 	cds a 	[enter] #=> will cd into directory starting with the character "a" if there's only one ohterwise will list out directories that start with the character "a"  
 
+## Heroku Config Search (heroku:configs) Usage
+
+	-Simple Usage
+	heroku:configs "" app1_name
+	#> list all config vars for app1
+
+	heroku:configs ConfigVar app1_name
+	#> list config vars for app1 that match ConfigVar
+
+	-Search Multiple Config Variables At Once
+	heroku:configs ConfigVar1,PartialConfigVar2,ConfigVar3 app1_name 	
+	#> list config vars for app1 that match ConfigVar1, PartialConfigVar2 and ConfigVar3
+
+	-Search Multiple Config Variables At Once In Multiple Apps
+	heroku:configs ConfigVar1,PartialConfigVar2,ConfigVar3 app1_name app2_name partial_app3_name
+	#> list config vars for app1 app2 and app3 that match ConfigVar1, PartialConfigVar2 and ConfigVar3
+
 
 ## Contributing
 
--- heroku.sh
+++ heroku.sh
@@ -1,27 +1,69 @@
 alias hk=heroku
 
-function hcs {
-	SKIPED_VARIABLE_ATTRIBUTE=""
-	IFS='|'
-
-	for var in "$@"
-	do
-		if [[ $SKIPED_VARIABLE_ATTRIBUTE ]]; then
-			echo -e "\033[32mAPP CONFIG\033[39m: $var"
-			echo -e "=================================================="
-
-			ALL_CONFIGS=$(heroku config -a $var )
-
-			if [[ $1 ]]; then 
-				for config in $1  
-				do
-					echo "$ALL_CONFIGS" | grep -E $config
-				done
-			else
-				echo "$ALL_CONFIGS"
-			fi
-
-		fi
-		SKIPED_VARIABLE_ATTRIBUTE=true
-	done
+#Usage heroku:apps app1_partial_name app2_partial_name app3
+function heroku:apps {
+	if [[ $1 == "" ]]; then	
+		echo -e "\033[32mHeroku Apps\033[39m:"
+		echo "=================================================="
+		heroku apps
+	else
+		for var in "$@"
+		do
+			app=$(echo "$app" | xargs)
+			echo -e "\033[32mApps Matching $var\033[39m: $app"
+			echo "=================================================="
+			heroku apps | grep -E ^.*$var.* 
+		done
+		echo -e "\r"
+	fi
 }
+
+# Usage heroku:configs searchTerm1,AnotherTerm,partialTerm3 app1_partial_name app2 app3_partial_name
+function heroku:configs  {
+	SKIPED_VARIABLE_ATTRIBUTE=""
+	APPS=$( heroku apps | grep -o ^.* )
+	APP_COUNT=$( echo "$APPS" | grep -E -c ".*$2.*" )
+
+	if [[ 15 -ge $APP_COUNT ]]; then 
+		# Iterate Over Apps to Search
+		for var in "$@"; do
+			# Skip First Argument Since It Declare Term(s) To Be Searched In App Configs
+			if [[ $SKIPED_VARIABLE_ATTRIBUTE ]]; then
+				# Array Of Apps Tha Match App Search String  
+				var=$(echo "$var" | xargs)
+				array=( $( echo "$APPS" | grep $var ) ) 
+				echo -e "\033[4;32mApps Matching '$var' With Configs Matching '$1' \033[m"
+
+				for app in "${array[@]}"; do 
+					app=$(echo "$app" | xargs)
+					ALL_APP_CONFIGS=$( heroku config -a "$app" )
+					echo -e "\033[4;36m$app\033[m"
+
+
+					IFS=','
+					if [[ $1 ]]; then 
+						for config in $1; do
+							search_match_count=$( echo "$ALL_APP_CONFIGS" | grep -o -c "$config" ) 
+							if [ "$search_match_count" != "0" ]; then
+								echo -e "\033[0;37m$( echo "$ALL_APP_CONFIGS" | grep -i -E $config ) \033[m"
+							else
+								echo -e "\033[31m   * no config vars matching '$config' *   \033[39m"
+							fi
+						done
+					else
+						echo "$ALL_APP_CONFIGS"
+					fi
+				done 
+				echo -e "\r"
+			fi
+			SKIPED_VARIABLE_ATTRIBUTE=true
+			IFS='
+			'
+		done
+	else
+		echo -e "\033[1;30m ##    Be More Specific There Are More Than 15 APPS    ## \033[0m"
+		echo -e "\033[1;30m ##         Usage: heroku:configs [app_search_term]         ## \033[0m"
+	fi
+}
+
+
