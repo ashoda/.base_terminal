5caa0cd291feb7c508225c61dcdd2c89c5f8a634
Ashod <ashod@Ashods-MacBook-Air.local>
2013-10-13 13:06:38 -0700

    added bookmarking to cds and refactored some dup code

--- cds.sh
+++ cds.sh
@@ -1,83 +1,132 @@
 #Work In Progress
 function cds {	
-	if [[ $1 == "-s" ]]; then 
-		if [[ $2 != "" ]]; then
-			bookmark="$(pwd)/$2"
-		else
-			bookmark="$(pwd)/"
-		fi
-		updated=$( grep -v -o "^$bookmark$" "$BASE_DIR/.cds-bookmarks" )
-		echo "$updated" > "$BASE_DIR/.cds-bookmarks"
-		echo "$bookmark" >> "$BASE_DIR/.cds-bookmarks"
-	elif [[ $1 == "-" ]]; then
+	
+	# view and or cd into bookmark
+	if [[ $1 == "-" ]]; then
+		
 		array=( $( cat "$BASE_DIR/.cds-bookmarks" ) )	
-		index=1
+		
 		echo -e "\033[0;32mBookmarked Directories: \033[m"
+		indexed_list $array
+		select_directory $array
+		
+	# add pwd to bookmarks or specify directory within the current directory
+	elif [[ $1 == "-a" ]]; then 
 
-		for i in "${array[@]}" 
-		do
-				echo -e "\033[32m$index\033[39m: $i"
-				((index++))
-		done
+		if [[ $2 != "" ]]; then
+			BOOKMARK="$(pwd)/$2"
+		else
+			BOOKMARK="$(pwd)/"
+		fi
 
-		printf "\e[1;30mSelect one Or press [enter] to exit > \e[m " 
-		read -r DIR
+		UPDATED=$( grep -v -o "^$BOOKMARK$" "$BASE_DIR/.cds-bookmarks" )
+		
+		echo "$UPDATED" > "$BASE_DIR/.cds-bookmarks"
+		echo "$BOOKMARK" >> "$BASE_DIR/.cds-bookmarks"
+		echo -e  "\033[1;32mbookmark added: \033[m $BOOKMARK"
 
-		if [ "$DIR" != "" ]
-			then cd "${array[$(( $DIR - 1 ))]}"
-		fi	
-
+	# delete current pwd from bookmarks
 	elif [[ $1 == "-d" ]]; then
-		unbookmark="$(pwd)/"
-		updated=$( grep -v -o "^$unbookmark$" "$BASE_DIR/.cds-bookmarks" )
-		echo "$updated" > "$BASE_DIR/.cds-bookmarks"
-	elif [[ $1 == "-da" ]]; then
+		
+		BOOKMARK="$(pwd)/"
+		UPDATED=$( grep -v -o "^$BOOKMARK$" "$BASE_DIR/.cds-bookmarks" )
+
+		echo "$UPDATED" > "$BASE_DIR/.cds-bookmarks"
+		echo -e  "\033[1;31mbookmark deleted: \033[m $BOOKMARK"
+
+	# delete all bookmarks
+	elif [[ $1 == "-da" ]]; then 
+
 		echo "" > "$BASE_DIR/.cds-bookmarks"
+		echo -e  "\033[1;31mbookmarks cleared\033[m"
+
+	elif [[ "$1" =~ ^-.*  ]]; then
+
+		echo "bash: cds: $1: invalid option"
+		echo "cds: usage: cd [-|-s|-d|-da] [dir]"
+
 	else
 
 	SEARCH=$1
-	EXACTMATCH_COUNT=$( ls -d */ .*/ | grep ^$1$ -c 	 )
-	PARTIALMATCH_COUNT=$( ls -d */ .*/ | grep -i ^$1.*$2/$ -c  )
+	EXACTMATCH_COUNT=$( ls -d */ .*/ | grep ^$SEARCH$ -c 	 )
+	PARTIALMATCH_COUNT=$( ls -d */ .*/ | grep -i ^$SEARCH.*$2/$ -c  )
 
-	IFS='/
+	set_ifs '/
 	'
+
 	# If input is numeric
 	if [ "$SEARCH" -eq "$SEARCH" ] 2>/dev/null; then
+	
 		array=(`ls -d */ `)
 		cd "${array[ $SEARCH - 1 ]}"
+	
 	# If input matches a directory name
 	elif [ $EXACTMATCH_COUNT == "1" ]; then 
-		cd $1		
+		
+		cd $SEARCH		
+
 	else
+		
 		# If partial match of one directory
 		if [ $PARTIALMATCH_COUNT == "1" ] ;
 			then
-			cd $( ls -d */ .*/ | grep -i ^$1.*$2/$ )
+			cd $( ls -d */ .*/ | grep -i ^$SEARCH.*$2/$ )
+		
 		# If partial match of one directory	
 		elif [ "$PARTIALMATCH_COUNT" -gt "1" ] ; then
-			if [[ "$1" =~ ^\..* ]]; then
-				array=( $( ls -d */ .*/ | grep -o -i ^$1.*$2/$ ) )
+			
+			# include hidden folders if first character of search term is a '.'
+			if [[ "$SEARCH" =~ ^\..* ]]; then
+				array=( $( ls -d */ .*/ | grep -o -i ^$SEARCH.*$2/$ ) )
 			else
-				array=( $( ls -d */ | grep -o -i ^$1.*$2/$ ) )
+				array=( $( ls -d */ | grep -o -i ^$SEARCH.*$2/$ ) )
 			fi
-			index=1
-			for i in "${array[@]}" 
-			do
-					echo -e "\033[32m$index\033[39m: $i"
-					((index++))
-			done
 
-			printf "\e[1;30mSelect one Or press [enter] to exit > \e[m " 
-			read -r DIR
+			indexed_list $array
 
-			if [ "$DIR" != "" ]
-				then cd "${array[$(( $DIR - 1 ))]}"
-			fi	
+			select_directory $array
+
 		else 
-			cd $1	
+			cd $SEARCH
 		fi
 	fi
-	IFS='
-	'
+
+	set_ifs
+	
 	fi
+}
+
+function indexed_list {
+
+	array=$1
+	index=1
+	for i in "${array[@]}"
+	do
+		echo -e "\033[32m$index\033[39m: $i"
+		((index++))
+	done
+
+}
+
+function select_directory {
+
+	array=$1
+	printf "\e[1;30mSelect one Or press [enter] to exit > \e[m " 
+	read -r DIR
+
+	if [ "$DIR" != "" ]
+		then cd "${array[$(( $DIR - 1 ))]}"
+	fi	
+
+}
+
+function set_ifs {
+
+	if [[ $1 ]]; then
+		IFS=$1
+	else
+		IFS='
+		'
+	fi
+
 }
