d1528371a61c87d1879f13b893df252072c1b577
Ashod Ayanyan <aayanyan@gmail.com>
2014-09-25 12:07:40 -0700

    adding - set_env shortcuts

--- rails.sh
+++ rails.sh
@@ -3,9 +3,13 @@ alias rs='rails s'
 alias rd='rails dbconsole'
 alias rk='bin/rake'
 alias rkdbc='bin/rake db:create'
-alias rkdbm='bin/rake db:migrate'
+alias rkdbm='set_test bin/rake db:migrate && set_dev bin/rake db:migrate'
 alias bi='bundle install'
 alias bu='bundle update'
+
+alias set_prod='RAILS_ENV=production'
+alias set_test='RAILS_ENV=test'
+alias set_dev='RAILS_ENV=development'
 
 function rails? {
   echo "rc    -> rails c"
@@ -16,4 +20,7 @@ function rails? {
   echo "rkdbm -> bin/rake db:migrate"
   echo "bi    -> bundle install"
   echo "bu    -> bundle update"
+  echo "set_prod -> RAILS_ENV=production"
+  echo "set_dev -> RAILS_ENV=development"
+  echo "set_test -> RAILS_ENV=test"
 }
