59c46df48d7542063c419e8be061e6b489864727
Ashod Ayanyan <aayanyan@gmail.com>
2014-09-25 12:13:01 -0700

    added git commit -m and -am git git shortcuts

--- git.sh
+++ git.sh
@@ -13,6 +13,8 @@ alias gd='git diff'
 alias ga='git add'
 alias ga.='git add .'
 alias gc='git commit'
+alias gcm='git commit -m'
+alias gcam='git commit -am'
 alias gf='git fetch'
 alias gm='git merge'
 alias gbl='git branch -v'
@@ -27,21 +29,23 @@ alias gcom='git checkout master'
 
 
 function git? {
-  echo "g     -> git"
-  echo "gs    -> git status"
-  echo "gd    -> git diff"
-  echo "ga    -> git add"
-  echo "ga.   -> git add ."
-  echo "gc    -> git commit"
-  echo "gf    -> git fetch"
-  echo "gm    -> git merge"
-  echo "gbl   -> git branch -v"
-  echo "grl   -> git remote -v"
-  echo "gfo   -> git fetch origin"
-  echo "gco   -> git checkout"
-  echo "gcom   -> git checkout master"
-  echo "gcob  -> git checkout -b"
-  echo "gpll  -> git pull"
-  echo "gpsh  -> git push"
-  echo "gsh   -> git stash"
+  echo "g           -> git"
+  echo "gs          -> git status"
+  echo "gd          -> git diff"
+  echo "ga          -> git add"
+  echo "ga.         -> git add ."
+  echo "gc          -> git commit"
+  echo "gcm         -> git commit -m"
+  echo "gcam        -> git commit -am"
+  echo "gf          -> git fetch"
+  echo "gm          -> git merge"
+  echo "gbl         -> git branch -v"
+  echo "grl         -> git remote -v"
+  echo "gfo         -> git fetch origin"
+  echo "gco         -> git checkout"
+  echo "gcom        -> git checkout master"
+  echo "gcob        -> git checkout -b"
+  echo "gpll        -> git pull"
+  echo "gpsh        -> git push"
+  echo "gsh         -> git stash"
 }
--- rails.sh
+++ rails.sh
@@ -12,15 +12,15 @@ alias set_test='RAILS_ENV=test'
 alias set_dev='RAILS_ENV=development'
 
 function rails? {
-  echo "rc    -> rails c"
-  echo "rs    -> rails s"
-  echo "rd    -> rails dbconsole"
-  echo "rk    -> bin/rake"
-  echo "rkdbc -> bin/rake db:create"
-  echo "rkdbm -> bin/rake db:migrate"
-  echo "bi    -> bundle install"
-  echo "bu    -> bundle update"
-  echo "set_prod -> RAILS_ENV=production"
-  echo "set_dev -> RAILS_ENV=development"
-  echo "set_test -> RAILS_ENV=test"
+  echo "rc          -> rails c"
+  echo "rs          -> rails s"
+  echo "rd          -> rails dbconsole"
+  echo "rk          -> bin/rake"
+  echo "rkdbc       -> bin/rake db:create"
+  echo "rkdbm       -> bin/rake db:migrate"
+  echo "bi          -> bundle install"
+  echo "bu          -> bundle update"
+  echo "set_prod    -> RAILS_ENV=production"
+  echo "set_dev     -> RAILS_ENV=development"
+  echo "set_test    -> RAILS_ENV=test"
 }
