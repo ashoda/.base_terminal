14170287d0d9cd3744f2a95797976bc7a8c1d2a7
Ashod Ayanyan <aayanyan@gmail.com>
2015-03-29 02:24:18 -0700

    Modified shortcut functionality

--- base.sh
+++ base.sh
@@ -7,13 +7,13 @@ BASE_DIR=`dirname $BASH_SOURCE`
 [ ! -n "$BASE_TERMINAL_SHORTCUTS" ] && BASE_TERMINAL_SHORTCUTS=()
 
 function bt:add_shortcut {
-	BASE_TERMINAL_SHORTCUTS+=("${IRed}[$3]${RCol}\t${Gre}$1${RCol} \t\t${Yel}=>${RCol}\t $2")
-	eval "sc:${3}() { sc $3; }"
+	BASE_TERMINAL_SHORTCUTS+=("${IRed}[$3]${RCol} ${Gre}$1${RCol} ${Yel} ${RCol} $2")
+	eval "??:${3}() { sc $3; }"
 	alias "$1?"="echo -e ${2}"
 	alias $1="$2"
 }
 function bt:add_function {
-  BASE_TERMINAL_SHORTCUTS+=("${IRed}[$3]${RCol}\t${Gre}$1${RCol} \t\t${Yel}=>${RCol}\t $2")
+  BASE_TERMINAL_SHORTCUTS+=("${IRed}[$3]${RCol} ${Gre}$1${RCol} ${Yel} ${RCol} $2")
   eval "sc:${3}() { sc $3; }"
   alias "$1?"="echo -e ${2}"
 }
@@ -27,7 +27,7 @@ function bt:add_function {
 export CLICOLOR=1
 export LSCOLORS=ExFxCxDxBxegedabagacad
 
-function shortcuts? {
+function ?? {
 	for var in "${BASE_TERMINAL_SHORTCUTS[@]}"
 	do
 		echo -e "${var}";
